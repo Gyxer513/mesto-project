db0179cc816907f2715d4a3ff5d42f91294731b1
Gyxer513 <fpanchuk@yandex.ru>
2022-05-27 17:00:20 +0300

    Добавлены интерактивные функции

--- scripts/index.js
+++ scripts/index.js
@@ -1,88 +1,98 @@
-const popup = document.querySelector(".popup");
-const openButton = document.querySelector(".profile__button-name");
-const closeButton = document.querySelector(".popup__container-button");
-const firstInput = document.querySelector("#first-input");
-const secondInput = document.querySelector("#second-input");
-const scontainer = document.querySelector('#elements');
-const elementImage = document.querySelector('.elements__image');
-const zoom = document.querySelector('.zoom');
-const zoomImage = document.querySelector('.zoom__image');
-const zoomSignature = document.querySelector('.zoom__signature');
-const zcloseButton = document.querySelector('.zoom__container-button');
-const profileName = document.querySelector('.profile__name');
-const profileSubname = document.querySelector('.profile__subname');
-const submitprofileButton = document.querySelector('.popup__button-save');
-const placeButton = document.querySelector('.profile__button-place');
+/* ********** ПЕРЕМЕННЫЕ ********** */
+const profileName = document.querySelector(".profile__name");
+const profileSubname = document.querySelector(".profile__subname");
+const containerTemplate = document.querySelector("#element");
+const elementImage = document.querySelector(".element__image");
+const zoomImage = document.querySelector(".zoom__image");
+const zoomSignature = document.querySelector(".zoom__signature");
+const profileForm = document.querySelector(".popup__form_js-profile");
+const placeForm = document.querySelector(".popup__form_js-place");
 
+/* **** ОБЪЯВЛЯЕМ ВСЕ ПОПАПЫ **** */
+const popupProfile = document.querySelector(".popup_js-edit-profile");
+const popupPlace = document.querySelector(".popup_js-add-place");
+const popupZoom = document.querySelector(".zoom");
 
+/* **** ОБЪЯВЛЯЕМ ВСЕ КНОПКИ **** */
+const profileButton = document.querySelector(".profile__button-name");
+const addPlace = document.querySelector(".profile__button-place");
 
+/* *** */
+const profileCloseButton = document.querySelector(
+  ".popup__container-button_js-profile"
+);
+const placeCloseButton = document.querySelector(
+  ".popup__container-button_js-place"
+);
+const zoomCloseButton = document.querySelector(".zoom__container-button");
 
-/* ОТКРЫТИЕ И ЗАКРЫТИЕ ПОПАПА ИЗМЕНЕНИЕ ИМЕНИ */
-openButton.addEventListener("click", function () {
+/* *** */
+const profileSaveButton = document.querySelector(
+  ".popup__button-save_js-profile"
+);
+const placeSaveButton = document.querySelector(".popup__button-save_js-place");
+
+/* **** ИНПУТЫ **** */
+const inputName = document.querySelector("#name");
+const inputSubname = document.querySelector("#subname");
+const inputPlace = document.querySelector("#place");
+const inputPicture = document.querySelector("#picture");
+
+/* ********** ОТКРЫТИЕ ПОПАПОВ ********** */
+function openPopup(popup) {
   popup.classList.add("popup_opened");
-  submitprofileButton.classList.remove("placeAddButton");
-  submitprofileButton.classList.add("editButton")
-  console.log(profileName);
-  firstInput.value = profileName.textContent;
-  secondInput.value = profileSubname.textContent;
-
-  submitprofileButton.addEventListener("click", function () {
-    profileName.textContent = firstInput.value;
-    profileSubname.textContent = secondInput.value;
-    popup.classList.remove("popup_opened");
-    return createstartItems(items);
-  });
-
-});
-
-
-closeButton.addEventListener("click", function () {
-  popup.classList.remove("popup_opened");
-});
-
-
-
-let popupEdit = document.querySelector("popup_edit");
-function editProfile() {
-  profileName.value = nameValue.textContent;
-  profileSubname.value = jobValue.textContent;
-  activeButtonElement(buttonElement, formElementList);
-  openPopup(popupEdit);
 }
 
-
-
-
-
-/* ОТКРЫТИЕ И ЗАКРЫТИЕ ПОПАПА ДОБАВЛЕНИЕ МЕСТА*/
-let addButton = document.querySelector(".profile__button-place");
-
-addButton.addEventListener("click", function () {
-  popup.classList.add("popup_opened");
-  submitprofileButton.classList.remove("editButton");
-  submitprofileButton.classList.add("placeAddButton");
-
-  submitprofileButton.addEventListener("click", function () {
-    
-    const item = {};
-    item.name = firstInput.value;
-    item.link = secondInput.value;
-    scontainer.prepend(createstartItems(item));
-    submitprofileButton.removeEventListener("click", this.name, false);     
-    popup.classList.remove("popup_opened");
-    });
-
-
-  console.log(profileName);
-  firstInput.value = 'Введите название';
-  secondInput.value = 'Дай ссылку';  
-});
-
-
-closeButton.addEventListener("click", function () {
+/* ********** ЗАКРЫТИЕ ПОПАПОВ ********** */
+function closePopup(popup) {
   popup.classList.remove("popup_opened");
+}
+
+/* ********** ПОПАП ПРОФИЛЯ ********** */
+
+/* **** ОТКРЫВАЕМ **** */
+profileButton.addEventListener("click", function () {
+  openPopup(popupProfile);
+  /* **** ЗАПИСЫВАЕМ ЗНАЧЕНИЯ **** */
+  inputName.value = profileName.textContent;
+  inputSubname.value = profileSubname.textContent;
+  profileForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileSubname.textContent = inputSubname.value;
+    closePopup(popupProfile);
+  });
+});
+/* **** ЗАКРЫВАЕМ **** */
+profileCloseButton.addEventListener("click", function () {
+  closePopup(popupProfile);
 });
 
+/* ********** ПОПАП ДОБАВЛЕНИЯ МЕСТА ********** */
+
+/* **** ОТКРЫВАЕМ **** */
+addPlace.addEventListener("click", function () {
+  openPopup(popupPlace);
+  inputPlace.placeholder = "Название";
+  inputPicture.placeholder = "Ссылка на картинку";
+});
+/* **** ДОБАВЛЯЕМ НОВУЮ КАРТОЧКУ **** */
+function addNewCard(evt) {
+  evt.preventDefault();
+  const item = {};
+  item.name = inputPlace.value;
+  item.link = inputPicture.value;
+  containerTemplate.prepend(createstartItems(item));
+  closePopup(popupPlace);
+  inputPlace.value = "";
+  inputPicture.value = "";
+}
+placeForm.addEventListener("submit", addNewCard);
+
+/* **** ЗАКРЫВАЕМ **** */
+placeCloseButton.addEventListener("click", function () {
+  closePopup(popupPlace);
+});
 
 /* СОЗДАНИЕ ЭЛЕМЕНТОВ */
 const initialCards = [
@@ -111,40 +121,45 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
+
 function createstartItems(items) {
-  const scards = document.querySelector('#elements').content;
-  const elementsCard = scards.querySelector('.elements__place').cloneNode(true);
-  const dellButton =  elementsCard.querySelector('.elements__button-remove');
-  const cardImage =  elementsCard.querySelector('.elements__image');
+  const containerTemplate = document.querySelector("#element").content;
+  const elementsCard = containerTemplate
+    .querySelector(".element__place")
+    .cloneNode(true);
+  const buttonDellite = elementsCard.querySelector(".element__button-remove");
+  const cardImage = elementsCard.querySelector(".element__image");
   cardImage.src = items.link;
   cardImage.alt = items.name;
-  elementsCard.querySelector('.elements__name').textContent = items.name;
-  elementsCard.querySelector('.elements__heart').addEventListener('click', function (evt) { /* РАЛИЗОВАЛИ ЛАЙК */
-    evt.target.classList.toggle("elements__heart_active");
-   });
-   elementsCard.querySelector('.elements__button-remove').addEventListener ('click', function (){ /* УДАЛЕНИЕ КАРТОЧКИ */
+  elementsCard.querySelector(".element__name").textContent = items.name;
+  elementsCard
+    .querySelector(".element__heart")
+    /* РАЛИЗОВАЛИ ЛАЙК */
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("element__heart_active");
+    });
+  buttonDellite.addEventListener("click", function () {
     elementsCard.remove();
   });
-  cardImage.addEventListener ('click', function (){ /* ОТКРЫВАЕМ И ЗАКРЫВАЕМ ZOOOOOOM */
+  /* ОТКРЫВАЕМ И ЗАКРЫВАЕМ ZOOOOOOM */
+  cardImage.addEventListener("click", function () {
     zoomImage.src = items.link;
     zoomImage.alt = items.link;
     zoomSignature.textContent = items.name;
-    zoom.classList.add("zoom_active");
+    openPopup(popupZoom);
   });
-  zcloseButton.addEventListener("click", function () {
-    zoom.classList.remove("zoom_active");
+  zoomCloseButton.addEventListener("click", function () {
+    closePopup(popupZoom);
   });
   return elementsCard;
 }
-const cardsList = initialCards.map (function (items) {     /* БЕРЁМ ЭЛЕМЕНТЫ ИЗ МАССИВА */
+/* БЕРЁМ ЭЛЕМЕНТЫ ИЗ МАССИВА */
+const cardsList = initialCards.map(function (items) {
   return createstartItems(items);
 });
 
-function AddCard(something) {
-  scontainer.prepend(createstartItems(something));  
-};
+function addCard(something) {
+  containerTemplate.prepend(createstartItems(something));
+}
 
-scontainer.append (...cardsList);
-
-
-
+containerTemplate.append(...cardsList);
