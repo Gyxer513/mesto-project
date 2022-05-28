dd90877b8963ce7d1b1203a88920c4ca9be142b5
Gyxer513 <fpanchuk@yandex.ru>
2022-05-28 13:13:08 +0300

    Исправлены все замечания ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+/* **** ОБЪЯВЛЯЕМ ВСЕ ПОПАПЫ **** */
+const popupProfile = document.querySelector(".popup_js-edit-profile");
+const popupPlace = document.querySelector(".popup_js-add-place");
+const popupZoom = document.querySelector(".zoom");
+
 /* ********** ПЕРЕМЕННЫЕ ********** */
 const profileName = document.querySelector(".profile__name");
 const profileSubname = document.querySelector(".profile__subname");
@@ -5,26 +10,22 @@ const containerTemplate = document.querySelector("#element");
 const elementImage = document.querySelector(".element__image");
 const zoomImage = document.querySelector(".zoom__image");
 const zoomSignature = document.querySelector(".zoom__signature");
-const profileForm = document.querySelector(".popup__form_js-profile");
-const placeForm = document.querySelector(".popup__form_js-place");
-
-/* **** ОБЪЯВЛЯЕМ ВСЕ ПОПАПЫ **** */
-const popupProfile = document.querySelector(".popup_js-edit-profile");
-const popupPlace = document.querySelector(".popup_js-add-place");
-const popupZoom = document.querySelector(".zoom");
+const profileForm = popupProfile.querySelector(".popup__form");
+const placeForm = popupPlace.querySelector(".popup__form");
+const elements = document.querySelector('#elements');
 
 /* **** ОБЪЯВЛЯЕМ ВСЕ КНОПКИ **** */
 const profileButton = document.querySelector(".profile__button-name");
-const addPlace = document.querySelector(".profile__button-place");
+const placeAdd = document.querySelector(".profile__button-place");
 
-/* *** */
-const profileCloseButton = document.querySelector(
-  ".popup__container-button_js-profile"
+/* *** */ /* КЛАСС, КОНЕЧНО НУЖНО ПЕРЕИМЕНОВАТЬ, НО ТОГДА НУЖНО МЕНЯТЬ СТРУКТУРУ БЭМ */
+const profileCloseButton = popupProfile.querySelector(
+  ".popup__container-button"
 );
-const placeCloseButton = document.querySelector(
-  ".popup__container-button_js-place"
+const placeCloseButton = popupPlace.querySelector(
+  ".popup__container-button"
 );
-const zoomCloseButton = document.querySelector(".zoom__container-button");
+const zoomCloseButton = popupZoom.querySelector(".popup__container-button");
 
 /* *** */
 const profileSaveButton = document.querySelector(
@@ -56,13 +57,15 @@ profileButton.addEventListener("click", function () {
   /* **** ЗАПИСЫВАЕМ ЗНАЧЕНИЯ **** */
   inputName.value = profileName.textContent;
   inputSubname.value = profileSubname.textContent;
-  profileForm.addEventListener("submit", function (evt) {
+});
+function editProfile(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileSubname.textContent = inputSubname.value;
     closePopup(popupProfile);
-  });
-});
+};
+profileForm.addEventListener("submit", editProfile);
+
 /* **** ЗАКРЫВАЕМ **** */
 profileCloseButton.addEventListener("click", function () {
   closePopup(popupProfile);
@@ -71,10 +74,8 @@ profileCloseButton.addEventListener("click", function () {
 /* ********** ПОПАП ДОБАВЛЕНИЯ МЕСТА ********** */
 
 /* **** ОТКРЫВАЕМ **** */
-addPlace.addEventListener("click", function () {
+placeAdd.addEventListener("click", function () {
   openPopup(popupPlace);
-  inputPlace.placeholder = "Название";
-  inputPicture.placeholder = "Ссылка на картинку";
 });
 /* **** ДОБАВЛЯЕМ НОВУЮ КАРТОЧКУ **** */
 function addNewCard(evt) {
@@ -82,11 +83,10 @@ function addNewCard(evt) {
   const item = {};
   item.name = inputPlace.value;
   item.link = inputPicture.value;
-  containerTemplate.prepend(createstartItems(item));
+  elements.prepend(createstartItems(item));
   closePopup(popupPlace);
-  inputPlace.value = "";
-  inputPicture.value = "";
-}
+  placeForm.reset();
+};
 placeForm.addEventListener("submit", addNewCard);
 
 /* **** ЗАКРЫВАЕМ **** */
@@ -123,43 +123,42 @@ const initialCards = [
 ];
 
 function createstartItems(items) {
-  const containerTemplate = document.querySelector("#element").content;
-  const elementsCard = containerTemplate
-    .querySelector(".element__place")
-    .cloneNode(true);
+  
+  const elementsCard = containerTemplate.content.querySelector(".element__place").cloneNode(true);
   const buttonDellite = elementsCard.querySelector(".element__button-remove");
   const cardImage = elementsCard.querySelector(".element__image");
   cardImage.src = items.link;
   cardImage.alt = items.name;
   elementsCard.querySelector(".element__name").textContent = items.name;
-  elementsCard
-    .querySelector(".element__heart")
-    /* РАЛИЗОВАЛИ ЛАЙК */
+  elementsCard.querySelector(".element__heart")
+
+
+  /* РАЛИЗОВАЛИ ЛАЙК */
     .addEventListener("click", function (evt) {
       evt.target.classList.toggle("element__heart_active");
     });
   buttonDellite.addEventListener("click", function () {
     elementsCard.remove();
   });
-  /* ОТКРЫВАЕМ И ЗАКРЫВАЕМ ZOOOOOOM */
+
+  /* ОТКРЫВАЕМ ZOOOOOOM*/
   cardImage.addEventListener("click", function () {
     zoomImage.src = items.link;
     zoomImage.alt = items.link;
     zoomSignature.textContent = items.name;
     openPopup(popupZoom);
   });
-  zoomCloseButton.addEventListener("click", function () {
-    closePopup(popupZoom);
-  });
   return elementsCard;
 }
+/* ЗАКРЫВАЕМ ZOOOOOOM */
+zoomCloseButton.addEventListener("click", function () {
+  closePopup(popupZoom);
+});
 /* БЕРЁМ ЭЛЕМЕНТЫ ИЗ МАССИВА */
 const cardsList = initialCards.map(function (items) {
   return createstartItems(items);
 });
 
-function addCard(something) {
-  containerTemplate.prepend(createstartItems(something));
-}
+elements.prepend(...cardsList); /* Я СДЕЛАЛ ЭТО, НО ЗАЧЕМ??? */
 
-containerTemplate.append(...cardsList);
+
